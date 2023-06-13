bd7c6c5da2de1b27ca4e1b52686b7ea2634de3a7
atodev <tom@tbutler.net>
2023-06-14 10:58:12 +1200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,164 @@
+
+#run WOW Pat 20230614
+cd controlteam1
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteam1_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteam1_relationshipsTS.tsv
+#
+cd ../controlteam1email
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteam1email_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteam1email_relationshipsTS.tsv
+#
+cd ../controlteam1teamschat
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteams1teamschat_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteams1teamschat_relationships_TS.tsv
+#
+#
+cd ../controlteam2
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteam2_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteam2_relationshipsTS.tsv
+#
+cd ../controlteam2email
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteam2email_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteam2email_relationshipsTS.tsv
+#
+cd ../controlteam2teamschat
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteams2teamschat_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteams2teamschat_relationships_TS.tsv
+##
+cd ../controlteam3
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteam3_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteam3_relationshipsTS.tsv
+#
+cd ../controlteam3email
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteam3email_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteam3email_relationshipsTS.tsv
+#
+cd ../controlteam3teamschat
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteams3teamschat_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteams3teamschat_relationships_TS.tsv
+##
+cd ../controlteam4
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteam4_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteam4_relationshipsTS.tsv
+#
+cd ../controlteam4email
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteam4email_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteam4email_relationshipsTS.tsv
+#
+cd ../controlteam4teamschat
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/controlteams4teamschat_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/controlteams4teamschat_relationships_TS.tsv
+####
+cd ../trialteam1
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam1_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam1_relationships_TS.tsv
+#
+cd ../trialteam1email
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam1email_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam1email_relationships_TS.tsv
+#
+cd ../trialteam1teamschat
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam1teamschat_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam1teamschat_relationships_TS.tsv
+##
+cd ../trialteam2
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam2_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam2_relationships_TS.tsv
+#
+cd ../trialteam2email
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam2email_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam2email_relationships_TS.tsv
+#
+cd ../trialteam2teamschat
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam2teamschat_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam2teamschat_relationships_TS.tsv
+#
+cd ../trialteam3
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam3_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam3_relationships_TS.tsv
+#
+cd ../trialteam3email
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam3email_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam3email_relationships_TS.tsv
+#
+cd ../trialteam3teamschat
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam3teamschat_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam3teamschat_relationships_TS.tsv
+#
+cd ../trialteam4
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam4_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam4_relationships_TS.tsv
+#
+cd ../trialteam4email
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam4email_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam4email_relationships_TS.tsv
+#
+cd ../trialteam4teamschat
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam4teamschat_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam4teamschat_relationships_TS.tsv
+#
+cd ../trialteam5
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam5_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam5_relationships_TS.tsv
+#
+cd ../trialteam5email
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam5email_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam5email_relationships_TS.tsv
+#
+cd ../trialteam5teamschat
+patoolkit --config ../relationshipsTS.cfg
+patoolkit --config ../relationships.cfg
+mv relationships.tsv ../MarchPat/trialteam5teamschat_relationships.tsv
+mv relationships_TS.tsv ../MarchPat/trialteam5teamschat_relationships_TS.tsv
